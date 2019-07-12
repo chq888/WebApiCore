02bed74f9dfd4ca4496a54d7d640d4292c4c2e46
cuonghq612 <cuonghq612@gmail.com>
2019-07-13 00:02:11 +0700

    init

---
+++ WebApiCore2/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApiCore2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //other code omitted
+            services.AddApiVersioning(o =>
+            {
+                o.ReportApiVersions = true;
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = new ApiVersion(1, 0);
+
+                o.ApiVersionReader = ApiVersionReader.Combine(new HeaderApiVersionReader("X-Version"),
+                    new QueryStringApiVersionReader("ver"),
+                    new UrlSegmentApiVersionReader());
+            });
+
+    //        // added to the web api configuration in the application setup
+    //        var constraintResolver = new DefaultInlineConstraintResolver()
+    //        {
+    //            ConstraintMap =
+    //{
+    //    ["apiVersion"] = typeof( ApiVersionRouteConstraint )
+    //}
+    //        };
+    //        services.MapHttpAttributeRoutes(constraintResolver);
+
+    //        IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+    //        services.AddOptions();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
